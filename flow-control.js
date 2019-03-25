7898a8f5fd923c329cf43cbf7a5f9c071ada850e
PS <palshankar@outlook.com>
2019-03-25 02:23:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,16 +31,11 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case ((age >= 13) && (age <= 19)):
-    {
-      return("You are a teenager");
-      break;
-    }
-    default:
-    {
+    case (age > 19):
       return("You have an age");
-      break;
-    }
-  return;
+    case (age < 13):
+      return("You have an age");          
+    default:
+      return("You are a teenager");
   }
 }
